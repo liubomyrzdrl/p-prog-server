d46924b6022c7f339508c3d97eda76d70bd09604
liubomyrzdrl <zdrilyiliubomyr@gmail.com>
2024-05-21 18:42:51 +0300

    feat: add prisma

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,15 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { PrismaClient } from '@prisma/client'
 
+
+const prisma = new PrismaClient();
 class AuthController {
+
   async authTest(req: FastifyRequest, reply: FastifyReply) {
+    const user = await prisma.user.findFirst();
     reply.code(200).send({
       data: {
-        hello: "Auth",
+        hello: `Auth ${user?.username} email: ${user?.email}`,
       },
       message: "Test Auth route",
     });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { FastifyInstance } from "fastify";
 import authRouter from "./auth/authRouter";
 
 const router = async (fastify: FastifyInstance) => {
-    console.log('fastify', fastify)
     fastify.register(authRouter, { prefix: 'auth' });
 };
 
